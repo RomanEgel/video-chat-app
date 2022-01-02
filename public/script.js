3d50202fe8f625874dbc17a1ee1e47cdba4dc528
RomanEgel <egel.rr@edu.spbstu.ru>
2022-01-02 13:32:36 +0300

    use cloud turn

--- public/script.js
+++ public/script.js
@@ -64,7 +64,7 @@ function openConnection() {
     const servers = {
         iceServers: [
             {urls: 'stun:stun.l.google.com:19302?transport=udp'},
-            {urls: 'turn:numb.viagenie.ca', username: "webrtc@live.com", credential: "muazkh"}
+            {urls: 'turn:34.159.76.223:3478', username: "tuta", credential: "tuta"}
         ]
     };
 
