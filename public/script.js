469f5aebcf9522c03169cb465258a6cc24590f3b
RomanEgel <egel.rr@edu.spbstu.ru>
2022-01-08 12:25:33 +0300

    connect by wss and refactored signaling

--- public/script.js
+++ public/script.js
@@ -1,34 +1,253 @@
 let myVideoStream;
 const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
-const remoteVideo = document.createElement('video')
+var videos = []
 const chatGrid = document.getElementById('chat-box')
 const inviteButton = document.getElementById('invite-button')
 const stopVideoButton = document.getElementById('stopVideo')
 const muteButton = document.getElementById('muteButton')
-let lc;
-let dc;
-let rc;
-let isHost = window.confirm("Are you host?");
-const userId = isHost ? "host" : "client";
+let isHost = USER_ACTION == "CREATE"
+var streamingConnections = []
+var dataConnections = []
 myVideo.muted = true;
 const offerOptions = {
     offerToReceiveAudio: 1,
     offerToReceiveVideo: 1
-  };
+};
+const servers = {
+    iceServers: [
+        {urls: 'stun:stun.l.google.com:19302?transport=udp'},
+        {urls: 'turn:34.159.76.223:3478', username: "tuta", credential: "tuta"}
+    ]
+}; 
 let answer;
 
-const serverUrl = "https://speakingspace.online";  
+const WEB_SERVER = "https://speakingspace.online"
+const BACKEND_SERVER_WS = "wss://speakingspace.online";  
 
-navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-})
-.then(stream => {
-    myVideoStream = stream
-    addVideoStream(myVideo, stream)
-})
-.then(() => openConnection())
+var ws = new WebSocket(BACKEND_SERVER_WS + "/video-chat");
+var client = Stomp.over(ws);
+
+client.connect({}, function (sessionId) {
+    client.subscribe('/user/queue/client-events', function (message) {
+        console.log("GOT MESSAGE!!!!")
+        const userEvent = JSON.parse(message.body)
+        console.log(userEvent)
+        switch (userEvent.eventType) {
+            case "ROOM_CREATE":
+                handleRoomCreation(userEvent)
+                break;
+            case "USER_JOIN":
+                handleUserJoin(userEvent)
+                break;
+            case "USER_OFFER":
+                handleUserOffer(userEvent);
+                break;
+            case "USER_ANSWER":
+                handleUserAnswer(userEvent);
+                break;
+            case "CONNECTION_UPDATE":
+                handleConnectionUpdate(userEvent);
+                break;    
+            case "USER_QUIT":
+                break;                
+        }
+    }) 
+
+    if (isHost) {
+        client.send('/chat/server-events', {}, JSON.stringify(buildCreateEvent()))
+    } else {
+        client.send('/chat/server-events', {}, JSON.stringify(buildJoinEvent()))
+    }
+});   
+
+function buildCreateEvent() {
+    return {
+        "eventType" : "ROOM_CREATE",
+        "target": ROOM_ID,
+        "payload": USER_NAME
+    }
+}
+
+function buildJoinEvent() {
+    return {
+        "eventType": "USER_JOIN",
+        "target": ROOM_ID,
+        "payload": USER_NAME
+    }
+}
+
+function buildOfferEvent(target, offer) {
+    return {
+        "eventType" : "USER_OFFER",
+        "target": target,
+        "payload": JSON.stringify(offer)
+    }
+}
+
+function buildAnswerEvent(target, answer) {
+    return {
+        "eventType" : "USER_ANSWER",
+        "target": target,
+        "payload": JSON.stringify(answer)
+    }
+}
+
+function buildConnectionUpdateEvent(target, ice) {
+    return {
+        "eventType" : "CONNECTION_UPDATE",
+        "target": target,
+        "payload": JSON.stringify(ice)
+    }
+}
+
+
+function handleRoomCreation(userEvent) {
+    if (!isHost || ROOM_ID != userEvent.source) {
+        console.log("Got message that not supposed to receive")
+        return;
+    }
+
+    startVideo();
+}
+
+function handleUserJoin(userEvent) {
+    if (ROOM_ID != userEvent.source) {
+        console.log("Got message that not supposed to receive")
+        return;
+    }
+
+    sendOffer(userEvent.payload)
+}
+
+function handleUserOffer(userEvent) {
+    startVideo()
+        .then(e => sendAnswer(userEvent.source, userEvent.payload))
+}
+
+function handleUserAnswer(userEvent) {
+    var con = streamingConnections[userEvent.source]
+    if (!con) {
+        console.log("TARGET IS NOT FOUND. ABORTING")
+        return;
+    }
+
+    con.setRemoteDescription(JSON.parse(userEvent.payload))
+        .then(e => console.log("Remote description set!"))
+}
+
+function handleConnectionUpdate(userEvent) {
+    var con = streamingConnections[userEvent.source]
+    if (!con) {
+        console.log("TARGET IS NOT FOUND. ABORTING")
+        return;
+    }
+
+    con.addIceCandidate(JSON.parse(userEvent.payload))
+}
+
+function sendAnswer(target, offer) {
+    createAnswer(target, offer)
+        .then(answer => client.send("/chat/server-events", {}, JSON.stringify(buildAnswerEvent(target, answer))))
+}
+
+function sendOffer(target) {
+    createOffer(target)
+        .then(offer => client.send("/chat/server-events", {}, JSON.stringify(buildOfferEvent(target, offer))))
+}
+
+function sendConnectionUpdate(target, ice) {
+    if (!ice) {
+        console.log("All ice candidates have been sent!")
+    }
+
+    client.send("/chat/server-events", {}, JSON.stringify(buildConnectionUpdateEvent(target, ice)))
+}
+
+function createOffer(target) {
+    chatGrid.appendChild(document.createTextNode('Opening connection!\n'))
+    
+    var lc = new RTCPeerConnection(servers)
+    lc.onicecandidateerror = e => {console.log("Candidate error"); console.log(e)}
+    lc.onnegotiationneeded = e => {console.log("Negotiation needed"); console.log(e)}
+    lc.onsignalingstatechange = e => {console.log("Signaling state changed"); console.log(e)}
+    var dc = lc.createDataChannel("chat")
+    dc.onmessage = e => chatGrid.append('Message: ' + e.data + '\n')
+    dc.onopen = e => chatGrid.appendChild(document.createTextNode('User connected!\n'))
+
+    lc.onicecandidate = e => sendConnectionUpdate(target, e.candidate)
+    myVideoStream.getTracks().forEach(track => lc.addTrack(track, myVideoStream));
+    lc.ontrack = e => {
+        var remoteVideo = videos[target]
+        if (!remoteVideo) {
+            remoteVideo = document.createElement("video")
+            videos[target] = remoteVideo
+        }
+
+        if (remoteVideo.srcObject !== e.streams[0]) {
+            addVideoStream(remoteVideo, e.streams[0])   
+            console.log('received remote stream');
+        }
+    }
+
+    
+    streamingConnections[target] = lc
+    dataConnections[target] = dc           
+    return lc.createOffer(offerOptions)
+            .then(o => {
+                lc.setLocalDescription(o)
+                return o
+            })
+}
+
+function createAnswer(target, offer) {
+    var rc = new RTCPeerConnection(servers)
+    rc.setRemoteDescription(JSON.parse(offer)).then(e => console.log("Set remote description"))
+    rc.onicecandidateerror = e => {console.log("Candidate error"); console.log(e)}
+    rc.onnegotiationneeded = e => {console.log("Negotiation needed"); console.log(e)}
+    rc.onsignalingstatechange = e => {console.log("Signaling state changed"); console.log(e)}
+    var dc;
+    rc.ondatachannel = e => {
+        dc = e.channel
+        dc.onmessage = e => chatGrid.append('Message: ' + e.data + '\n')
+        dc.onopen = e => chatGrid.appendChild(document.createTextNode('User connected!\n'))
+    }
+    rc.onicecandidate = e => sendConnectionUpdate(target, e.candidate)
+    myVideoStream.getTracks().forEach(track => rc.addTrack(track, myVideoStream));
+    rc.ontrack = e => {
+        var remoteVideo = videos[target]
+        if (!remoteVideo) {
+            remoteVideo = document.createElement("video")
+            videos[target] = remoteVideo
+        }
+
+        if (remoteVideo.srcObject !== e.streams[0]) {
+            addVideoStream(remoteVideo, e.streams[0])   
+            console.log('received remote stream');
+        }
+    }
+
+    streamingConnections[target] = rc
+    dataConnections[target] = dc
+
+    return rc.createAnswer()
+                .then(a => {
+                    rc.setLocalDescription(a)
+                    return a;
+                })
+}
+
+function startVideo() {
+    return navigator.mediaDevices.getUserMedia({
+                audio: true,
+                video: true,
+            })
+            .then(stream => {
+                myVideoStream = stream
+                addVideoStream(myVideo, stream)
+            })
+}
+
 
 function addVideoStream(video, stream) {
     video.srcObject = stream
@@ -58,123 +277,16 @@ function iOS() {
     // iPad on iOS 13 detection
     || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
   }
+  
 
-inviteButton.onclick = connectToServer
 muteButton.onclick = e => {
-    myVideoStream.getAudioTracks()[0].enabled ^= true
+    //myVideoStream.getAudioTracks()[0].enabled ^= true
+    client.send("/chat/server-events", {}, JSON.stringify({"eventType": "TEST"}))
 }
+
+
 stopVideoButton.onclick = e => {
-    if (isHost) {
-        lc.close()
-        axios({
-            method: 'post',
-            url: serverUrl + "/close-room",
-            data: {
-                "roomId": ROOM_ID,
-                "userId": userId
-            }
-        })
-    } else {
-        rc.close()
-    }
+    myVideoStream.getVideoTracks()[0].enabled ^= true
 }
 
-function openConnection() {
-    const servers = {
-        iceServers: [
-            {urls: 'stun:stun.l.google.com:19302?transport=udp'},
-            {urls: 'turn:34.159.76.223:3478', username: "tuta", credential: "tuta"}
-        ]
-    };
-
-    if (isHost) {
-        chatGrid.appendChild(document.createTextNode('Opening connection!\n'))
-        lc = new RTCPeerConnection(servers)
-        lc.onicecandidateerror = e => {console.log("Candidate error"); console.log(e)}
-        lc.onnegotiationneeded = e => {console.log("Negotiation needed"); console.log(e)}
-        lc.onsignalingstatechange = e => {console.log("Signaling state changed"); console.log(e)}
-        dc = lc.createDataChannel("chat")
-        dc.onmessage = e => chatGrid.append('Message: ' + e.data + '\n')
-        dc.onopen = e => chatGrid.appendChild(document.createTextNode('User connected!\n'))
-
-        lc.onicecandidate = e => console.log(JSON.stringify(lc.localDescription))
-        myVideoStream.getTracks().forEach(track => lc.addTrack(track, myVideoStream));
-        lc.ontrack = e => {
-            if (remoteVideo.srcObject !== e.streams[0]) {
-                addVideoStream(remoteVideo, e.streams[0])   
-                console.log('received remote stream');
-            }
-        }
-    
-        lc.createOffer(offerOptions)
-            .then(o => lc.setLocalDescription(o))
-            .then(a => chatGrid.appendChild(document.createTextNode('Local description updated!\n')))            
-    } else {
-        rc = new RTCPeerConnection(servers)
-        rc.onicecandidateerror = e => {console.log("Candidate error"); console.log(e)}
-        rc.onnegotiationneeded = e => {console.log("Negotiation needed"); console.log(e)}
-        rc.onsignalingstatechange = e => {console.log("Signaling state changed"); console.log(e)}
-        rc.ondatachannel = e => {
-            dc = e.channel
-            dc.onmessage = e => chatGrid.append('Message: ' + e.data + '\n')
-            dc.onopen = e => chatGrid.appendChild(document.createTextNode('User connected!\n'))
-        }
-        rc.onicecandidate = e => console.log(JSON.stringify(rc.localDescription))
-        myVideoStream.getTracks().forEach(track => rc.addTrack(track, myVideoStream));
-        rc.ontrack = e => {
-            if (remoteVideo.srcObject !== e.streams[0]) {
-                addVideoStream(remoteVideo, e.streams[0])   
-                console.log('received remote stream');
-            }
-        }
-    }  
-}
-
-function connectToServer() {
-    if (isHost) {
-        axios({
-            method: 'post',
-            url: serverUrl + "/create-room",
-            data: {
-                "roomId": ROOM_ID,
-                "userId": userId,
-                "offer": JSON.stringify(lc.localDescription)
-            }
-        })
-        .then(response => console.log("SUCCESS !"))
-        .then(() => listenToServer())
-    } else {
-        axios({
-            method: 'post',
-            url: serverUrl + "/join-room",
-            data: {
-                "roomId": ROOM_ID,
-                "userId": userId,
-            }
-        })
-        .then(response => {console.log("OFFER:"); console.log(response.data); rc.setRemoteDescription(response.data);})
-        .then(e => console.log('Description set'))
-        .then(e => rc.createAnswer())
-        .then(a => rc.setLocalDescription(a))
-        .then(() => listenToServer())
-    }
-}
-
-function listenToServer() {
-    var ws = new SockJS(serverUrl + "/chat/connect");
-    var client = Stomp.over(ws);
-    client.connect({}, function (frame) {
-        if (isHost) {
-            client.subscribe('/topic/room-' + ROOM_ID, function (message) {
-                const chatEvent = JSON.parse(message.body)
-                answer = JSON.parse(chatEvent.answer)
-                console.log("ANSWER:")
-                console.log(answer)
-                lc.setRemoteDescription(answer)
-                    .then(e => console.log("Connection set!"))
-            });
-        } else {
-            client.send("/chat/connect", {}, JSON.stringify({"roomId": ROOM_ID, "userId": userId, "answer": JSON.stringify(rc.localDescription)}));
-        }
-      });
-}
+inviteButton.onclick = e => window.alert("JOIN LINK: " + WEB_SERVER + "/?room=" + ROOM_ID)
--- server.js
+++ server.js
@@ -5,9 +5,35 @@ const { v4: uuidv4 } = require("uuid");
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+const session = require('express-session');
+app.use(session({secret: 'mySecret', resave: false, saveUninitialized: false}));
 
-app.get('/', (req, res) => res.redirect('/' + uuidv4()))
-app.get('/:room', (req, res) => res.render('room', {room: req.params.room}))
+app.get('/', (req, res) => {
+    res.render('lobby', {room: req.query.room})
+})
+app.get('/create-room', (req, res) => {
+    var username = req.query.name
+    req.session.context = {"username": username, "action": "CREATE"};
+    console.log('GOT create request for user ' + username)
+    res.redirect('/rooms/' + uuidv4())
+})
+app.get('/join-room/:room', (req, res) => {
+    var username = req.query.name
+    req.session.context = {"username": username, "action": "JOIN"};
+    console.log('GOT join request for user ' + username)
+    res.redirect('/rooms/' + req.params.room)
+})
+app.get('/rooms/:room', (req, res) => {
+    if (!req.session.context) {
+        console.log("tuta")
+        res.redirect('/')
+        return
+    }
+    var name = req.session.context.username
+    var roomId = req.params.room
+    console.log("Redirecting user " + name + " to room " + roomId)
+    res.render('room', {room: roomId, username: name, action: req.session.context.action})
+})
 app.get('/greeting/:name', (req, res) => res.status(200).send("Hello " + req.params.name + "!"))
 
 server.listen(3000)
